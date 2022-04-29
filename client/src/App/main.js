3906f24b1fdb47cffc48244aeca6afe0870cb7a1
NcUndstnd <Ignatcov169@gmail.com>
2022-04-30 00:33:26 +0300

    added_started_packs

---
+++ client/src/App/main.js
@@ -0,0 +1,4 @@
+import '../styles/main.scss';
+
+let x = 1949;
+alert(x);
---
+++ client/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+var mode = "development";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
+module.exports = {
+  mode,
+  entry: "./src/App/main.js",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[hash][ext][query]",
+    clean: true,
+  },
+
+  devServer: {
+    hot: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/App/main.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+
+  module: {
+    rules: [
+      { test: /\.(html)$/, use: ["html-loader"] },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === "production" ? "asset" : "asset/resource",
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
