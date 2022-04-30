50dc61169cdfa2d29a51bec3b2ef4eb6b1fa7000
NcUndstnd <Ignatcov169@gmail.com>
2022-04-30 16:36:00 +0300

    add Material UI

--- client/src/App/components/Div.js
+++ client/src/App/components/Div.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import Button from "@mui/material/Button";
 
-function Div(){
-  return(
-    <div>
-      <h1>
-        Text created by React 18.1.0!
-      </h1>
-    </div>
-  )
+function Div() {
+  return (
+    <>
+      <div>
+        <h1>Text created by React 18.1.0!</h1>
+      </div>
+      <Button size="medium" variant="contained" color="secondary">
+        Button created by Material UI!)
+      </Button>
+    </>
+  );
 }
 
 export default Div;
