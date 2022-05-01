a755b3a7b0180bf33f36f0d79b2828c5e1deff5c
NcUnsdtnd <Ignatcov169@gmail.com>
2022-05-01 23:00:15 +0300

    table

--- client/src/App/components/body/table.js
+++ client/src/App/components/body/table.js
@@ -1,23 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { TextField } from "@mui/material";
+import { TextField, MenuItem } from "@mui/material";
 
 export default function Table(props) {
+  const [currency, setCurrency] = React.useState("");
+
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
+
   const array = [];
 
   for (let index = 0; index < props.size; index++) {
-    array[index] = <></>;
+    array[index] = <>{index}</>;
   }
+
+  const currencies = [
+    {
+      label: "ОАиП",
+      value: "ОАиП",
+    },
+    {
+      label: "СЯП",
+      value: "СЯП",
+    },
+    {
+      label: "КЯРД",
+      value: "КЯРД",
+    },
+    {
+      label: "СД",
+      value: "СД",
+    },
+    {
+      label: "АЛОЦВМ",
+      value: "АЛОЦВМ",
+    },
+  ];
 
   return (
     <div className="body">
       {array.map((con) => (
         <TextField
+          key={con}
           className="cell"
-          margin="normal"
-          color="common"
-          focused
-        />
+          select
+          value={currency}
+          onChange={handleChange}
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
       ))}
     </div>
   );
